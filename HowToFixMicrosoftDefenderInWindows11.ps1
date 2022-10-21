b15c11cf098c9ab173e3fffad1aade8905f8a671
ThatCyberGuy5 <61467648+cnenno@users.noreply.github.com>
2022-10-21 07:57:21 -0500

    How To Fix Microsoft Defender in Windows 11
    If Microsoft Defender has uninstalled itself, here are a few ways to get it back!

---
+++ HowToFixMicrosoftDefenderInWindows11.ps1
@@ -0,0 +1,7 @@
+﻿Get-AppxPackage MicrosoftSecHealthUI -AllUsers | Reset-AppxPackage
+Get-AppxPackage *Microsoft.Windows.SecHealthUI* | Reset-AppxPackage
+PowerShell -ExecutionPolicy Unrestricted -Command "& {$manifest = (Get-AppxPackage *Microsoft.Windows.SecHealthUI*).InstallLocation + '\AppxManifest.xml' ; Add-AppxPackage -DisableDevelopmentMode -Register $manifest}"
+Add-AppxPackage -Register -DisableDevelopmentMode "C:\Windows\SystemApps\Microsoft.Windows.SecHealthUI_cw5n1h2txyewy\AppXManifest.xml"
+add-appxpackage -disabledevelopmentmode -register ((Get-AppxPackage Microsoft.SecHealthUI -allusers).InstallLocation + '\AppxManifest.xml')
+Set-ExecutionPolicy Unrestricted
+Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
